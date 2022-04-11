d39a1f598714c44b79be48eb7c48ec70b73e9040
gezish <enggezahegn.w@gmail.com>
2022-04-11 19:41:49 +0300

    last

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- *  * main - prints the name of file
- *   * Return: Always 0 (Success)
- *    */
+ *  * main - print file name
+ *   *
+ *    * Return:void
+ *     */
 int main(void)
 {
 		printf("%s\n", __FILE__);
