7f5129e570912dae1854ceeedd16ed6b15d7dcc9
gezish <gwondachew@gmail.com>
2022-04-11 10:07:21 -0700

    last for betty fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,18 @@
-#ifndef _DOG_H_
-#define _DOG_H_
-
+#ifndef DOG_H
+#define DOG_H
 /**
- *  * struct dog - structure for a basic dog
- *   * @name: Dog's name
- *    * @age: Dog's age
- *     * @owner: Dog's owner
- *      * Description: data structure for dog
- *       */
-struct dog
+ *struct dog - a new type
+ *@name : string
+ *@age : float
+ *@owner : string
+ *Description: a new type that contains dog name, age, and owner.
+ */
+typedef struct dog
 {
-		char *name;
-			float age;
-				char *owner;
-};
-typedef struct dog dog_t;
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
